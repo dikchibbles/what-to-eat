be55e1081903afee369d269268b9367e890979ad
grisa <grisa@mail.ru>
2022-01-12 21:24:13 +0300

    Add show_recipe.html, and add_favorite functionality

--- main.py
+++ main.py
@@ -219,8 +219,45 @@ def dessert():
 
 
 @app.route('/recipe/<int:recipe_id>')
+@login_required
 def get_recipe(recipe_id):
-    pass
+    url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information"
+    headers = {
+        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        'x-rapidapi-key': "4122f3483amsh58a4641df90e077p13dbeejsn7d92b5bcd947"
+    }
+    recipe = requests.get(url, headers=headers).json()
+    category = ''
+    for key in list(recipe)[:8]:
+        if recipe[key]:
+            load()
+            category_list = segment(key)
+            category = ' '.join(category_list).lower()
+            break
+    dish_name = recipe['title']
+    try:
+        image = recipe['image']
+    except KeyError:
+        url = "https://bing-image-search1.p.rapidapi.com/images/search"
+        querystring = {"q": f"{dish_name}"}
+        headers = {
+            'x-rapidapi-host': "bing-image-search1.p.rapidapi.com",
+            'x-rapidapi-key': "4122f3483amsh58a4641df90e077p13dbeejsn7d92b5bcd947"
+        }
+        response = requests.request("GET", url, headers=headers,
+                                    params=querystring).json()
+        image = response['value'][0]['thumbnailUrl']
+    instructions = recipe['instructions']
+    ingredients = []
+    for ingredient in recipe['extendedIngredients']:
+        ingredients.append(ingredient['original'])
+    return render_template('show_recipe.html',
+                           dish_name=dish_name,
+                           image=image,
+                           instructions=instructions,
+                           ingredients=ingredients,
+                           category=category,
+                           current_user=current_user)
 
 
 @app.route('/add_favorite/<int:recipe_id>/<recipe_name>', methods=['GET', 'POST'])
