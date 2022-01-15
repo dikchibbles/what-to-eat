d7b89b982bc88d612b4f2b248dc32ab9bba7c220
grisa <grisa@mail.ru>
2022-01-15 09:23:41 +0300

    Fix footer to the bottom of the pages, add the confirm password functionality

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -12,7 +12,8 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     name = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Register")
 
 
